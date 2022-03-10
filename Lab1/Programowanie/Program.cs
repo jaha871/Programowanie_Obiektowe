8595063d2b738cbfc0d997caca3c2fe137bcbacf
Sebastian Halada <Sebastian.Halada@microsoft.wsei.edu.pl>
2022-03-10 09:07:02 +0100

    pierwsza wersja

---
+++ Lab1/Programowanie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 11, 2, 5, 6, 7, 45, 23, 1, 7 };
+            Console.WriteLine(findIndex(arr, -5));
+        }
+        static int findIndex(int[] arr, int k)
+        {
+            int index = -1;
+            int min = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i] && arr[i] > 10)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
