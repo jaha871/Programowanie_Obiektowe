2e388b14e85a8e4130c5bcfda277f6481f94bdd5
Sebastian Halada <Sebastian.Halada@microsoft.wsei.edu.pl>
2022-03-18 10:24:12 +0100

    Drugi

--- Lab_2/Lab_2/Program.cs
+++ Lab_2/Lab_2/Program.cs
@@ -6,7 +6,11 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        class Classroom
+        {
+            
         }
     }
 }
